e10e4d4e28f6659e0e10d415c281a307f322da8f
adypu <menongokul21@gmail.com>
2020-02-18 15:25:58 +0530

    activity life cycle

--- app/src/main/java/com/mav/myapplication/MainActivity.java
+++ app/src/main/java/com/mav/myapplication/MainActivity.java
@@ -3,12 +3,43 @@ package com.mav.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "onCreate",Toast.LENGTH_LONG).show();
     }
 }
